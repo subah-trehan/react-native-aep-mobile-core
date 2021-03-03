33ccaae5b10051dbc59bc57988167f4b52e94d61
trehan <trehan@adobe.com>
2021-03-03 14:41:56 +0530

    version modified

--- js/RNMobileCore.js
+++ js/RNMobileCore.js
@@ -6,10 +6,7 @@ import {NativeModules} from 'react-native';
 const RNMobileCore = NativeModules.RNMobileCore;
 
 module.exports = {
-  /**
-   * Returns the version of the ACPUserProfile extension
-   * @param  {string} Promise a promise that resolves with the extension verison
-   */
+  
   extensionVersion(): Promise<string> {
     return Promise.resolve(RNMobileCore.extensionVersion());
   },
